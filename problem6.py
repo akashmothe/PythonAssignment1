944f91706cf63f4149a1cfa2e76d3dfffc1a6dba
akashmothe <akash.mothe@ecolibriumenergy.com>
2022-01-21 10:08:05 +0530

    updated

--- problem6.py
+++ problem6.py
@@ -12,9 +12,14 @@ from collections import Counter
 
 def high_low_occurrence(l):
     res = Counter(l)
-    for i in res.elements():
-        print(res[i])
-    return Counter(l)
+    max_count = (0,0)
+    min_count = ()
+    for key,val in res.items():
+        if val > max_count[-1]:
+            max_count = (key,val)
+        # if val < min_count[-1]:
+        #     min_count = (key, val)
+    return max_count
 
 l = [7, 8, 4, 2, 4, 4, 1, 1, 7, 7, 2, 5]
 print(high_low_occurrence(l))
