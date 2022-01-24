101d2d61020cff77bac1a6be4b7b189906beb290
akashmothe <akash.mothe@ecolibriumenergy.com>
2022-01-24 12:16:02 +0530

    updated

--- problem6.py
+++ problem6.py
@@ -1,17 +1,27 @@
 from collections import Counter
 
+
 # def high_low_occurrence(l):
 #     res = Counter(l)
-#     max_count = (0,0)
-#     min_count = ()
-#     for key,val in res.items():
-#         if val > max_count[-1]:
-#             max_count = (key,val)
-#     return max_count
+#     return res.most_common(1    )
 
 def high_low_occurrence(l):
-    res = Counter(l)
-    return res.most_common(1)
+    max_ele = 0
+    max_frequency = 0
+    min_ele = 0
+    min_frequency = 0
+    # frequency = []
+    for i in l:
+        if i > max_ele and l.count(i) > max_frequency:
+            max_ele = i
+            max_frequency = l.count(i)
+        min_ele = i
+        min_frequency = l.count(i)
+        if i < min_ele and l.count(i) < min_frequency:
+            min_ele = i
+            min_frequency = l.count(i)
+    print(f"Highest occurring element {max_ele} occurs {max_frequency} times.")
+    print(f"Lowest occurring element {min_ele} occurs {min_frequency} times.")
 
 l = [7, 8, 4, 2, 4, 4, 1, 1, 7, 7, 2, 5]
-print(high_low_occurrence(l))
+high_low_occurrence(l)
--- problem7.py
+++ problem7.py
@@ -1,9 +1,12 @@
 
 def sort_by_column(arr,k):
-    sort_eles = []
-    for i in range(len(arr)):
-        
+    kth_column = [ele[k-1] for ele in arr]
+    kth_col_sorted = sorted(kth_column)
+    for i in range(len(kth_col_sorted)):
+        arr[i][k-1] = kth_col_sorted[i]
+    return arr
 
 
+k = int(input("Enter the column number: "))
 arr = [[39, 27, 11, 42], [10, 93, 91, 90], [54, 78, 56, 89], [24, 64, 20, 65]]
 print(sort_by_column(arr,3))
